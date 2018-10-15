592d4cfa7191e48873251170ad0e128433387ec1
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-10-15 00:22:57 -0400

    addressing failure to build in pipeline

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 import { FormControl, Validators } from '@angular/forms';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-login',
